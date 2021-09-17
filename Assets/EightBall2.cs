04a12616551148116b9b4bd6d6cd666fd7b0e2c8
jtlaughton <60359046+jtlaughton@users.noreply.github.com>
2021-09-16 20:15:53 -0500

    Final Iteration

--- Assets/EightBall2.cs
+++ Assets/EightBall2.cs
@@ -9,7 +9,7 @@ public class EightBall2 : MonoBehaviour
                         "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes",
                          "Signs point to yes", "Reply hazy, try again", "Ask again later", "Better not tell you now",
                          "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no",
-                         "My sources say no", "Outlook not so good", "Very dountful"};
+                         "My sources say no", "Outlook not so good", "Very doubtful"};
     // Start is called before the first frame update
 
     [SerializeField]
